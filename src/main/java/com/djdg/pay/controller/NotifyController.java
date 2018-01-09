bf2e17be01368d3647213f2a3c07a3c8cf091269
Demon <zhuangdimeng@hey900.com>
2018-01-09 17:26:55 +0800

    回调

--- src/main/java/com/djdg/pay/controller/NotifyController.java
+++ src/main/java/com/djdg/pay/controller/NotifyController.java
@@ -1,10 +1,14 @@
 package com.djdg.pay.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.djdg.pay.model.dto.BaseXmlDTO;
+import com.djdg.pay.model.dto.WxNotificationDTO;
 import com.djdg.pay.model.vo.WxPayResponseVO;
 import com.djdg.pay.service.NotifyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.SortedMap;
 
 /**
  * @Description:
@@ -27,14 +31,10 @@ public class NotifyController {
      */
     @PostMapping(value = "wx/{payMethod}/{businessAppId}", produces = "text/xml;charset=utf-8")
     public WxPayResponseVO wxNotify(@RequestBody BaseXmlDTO baseXmlDTO, @PathVariable("payMethod") String payMethod, @PathVariable("businessAppId") String businessAppId) {
-        /*SortedMap<String, String> notifyParams = baseXmlDTO.get();
+        SortedMap<String, String> notifyParams = baseXmlDTO.get();
         WxNotificationDTO notification = JSONObject.parseObject(JSONObject.toJSONString(notifyParams), WxNotificationDTO.class);
         notification.setNotify_params(notifyParams);
-        return notifyService.processWxNotification(notification, payMethod, businessAppId);*/
-        WxPayResponseVO response = new WxPayResponseVO();
-        response.setReturn_code("SUCCESS");
-        response.setReturn_msg("DONE");
-        return response;
+        return notifyService.processWxNotification(notification, payMethod, businessAppId);
     }
 
 }
